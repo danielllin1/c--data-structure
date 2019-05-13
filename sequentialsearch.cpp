bbfe0dd9a95e996db81eb81212882fb8a26be668
danielllin1 <47834105+danielllin1@users.noreply.github.com>
2019-05-12 20:37:58 -0400

    Update sequentialsearch.cpp

--- sequentialsearch.cpp
+++ sequentialsearch.cpp
@@ -3,7 +3,11 @@
 #include<vector>
 #include<string>
 
+//This is a sequential search file I created to search words in a dictionary("sgb-words.txt") of 5756 words.
+
 using namespace std;
+
+int myTest(vector<string>testVector,int number,string key);
 
 int main(){
     ifstream myFile;
@@ -20,21 +24,55 @@ int main(){
     }
     cout << "Enter the word you want to find in the word list" << endl;
     cin >> findWord;
-    if(findWord.size()!= 5){
-        cout << "Re-enter a five-letter word for searching." << endl;
-    }
     while(findWord.size() != 5){
+        cout << "Re-enter a five-letter word for searching." << endl;
         cin >> findWord;
     }
-    return 0;
+    //This is a simple loop to perform sequential search.
+    for(int i = 0; i < myVector.size(); i++){
+        if(myVector[i] == findWord){
+            wordLocation = i;
+        }
+    }
+    //Checking the boundary.
+    if(wordLocation == 0){
+        cout << "The word you entered can not be found in the dictionary." << endl;
+    }
+    //printing out the index of the word you entered.
+    else{
+    cout << "The index number of your word is: " << wordLocation << endl;
+    }
+    //-------------.
+    //myTest function is what I learned from Data structure class.
+    myTest(myVector,myVector.size(),findWord);
+        return 0;
+}
+int myTest(vector<string>testVector,int number,string key){
+    int i = 0;
+    while(i < number && testVector[i] != key){
+         i++;
+    }
+    //Checking the boundary.
+    if(i == number && key != testVector[i]){
+        cout << "The word you entered can not be found in the dictionary." << endl;
+    }
+    else{
+    //printing out the index of the word you entered.
+        cout << "The index number of your word is: " << i << endl;
+    }
+    if(i < number){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 //The following are some additional information about sequential search by Dr.Kramer.
 /*Sequential Search.
 Three questions about the algorithm.
 Question 1: What if the first list position is 1, not 0?
 Yes, 
-If the first position is j , replace all occurrences of
-L[i ] with L[i + j ].
+If the first position is j , replace all occurrences of L[i ] with L[i + j ].
 
 Question 2: Can we always compare keys?
 Yes, 
