ba9efc3a06e2b111fa15a0a2523487a5a1d5daa0
danielllin1 <47834105+danielllin1@users.noreply.github.com>
2019-05-13 14:40:03 -0400

    Update sequentialsearch.cpp

--- sequentialsearch.cpp
+++ sequentialsearch.cpp
@@ -2,12 +2,15 @@
 #include<fstream>
 #include<vector>
 #include<string>
-
-//This is a sequential search file I created to search words in a dictionary("sgb-words.txt") of 5756 words.
+#include<random>
 
 using namespace std;
 
-int myTest(vector<string>testVector,int number,string key);
+int sequentialSearch(vector<string>testVector,int number,string key);
+int sentinelSearch(vector<string>testVector, int number,string key);
+int probabilitySearch(vector<string>testVector, int number,string key);
+int promote(vector<string>testVector, int i);
+int orderedSearch(vector<string>testVector, int number,string key);
 
 int main(){
     ifstream myFile;
@@ -28,38 +31,20 @@ int main(){
         cout << "Re-enter a five-letter word for searching." << endl;
         cin >> findWord;
     }
-    //This is a simple loop to perform sequential search.
-    for(int i = 0; i < myVector.size(); i++){
-        if(myVector[i] == findWord){
-            wordLocation = i;
-        }
-    }
-    //Checking the boundary.
-    if(wordLocation == 0){
-        cout << "The word you entered can not be found in the dictionary." << endl;
-    }
-    //printing out the index of the word you entered.
-    else{
-    cout << "The index number of your word is: " << wordLocation << endl;
-    }
-    //-------------.
-    //myTest function is what I learned from Data structure class.
-    myTest(myVector,myVector.size(),findWord);
-        return 0;
+    sequentialSearch(myVector,myVector.size(),findWord);
+    sentinelSearch(myVector,myVector.size(),findWord);
+    probabilitySearch(myVector,myVector.size(),findWord);
+    orderedSearch(myVector,myVector.size(),findWord);
+    return 0;
 }
-int myTest(vector<string>testVector,int number,string key){
+
+int sequentialSearch(vector<string>testVector,int number,string key){
     int i = 0;
     while(i < number && testVector[i] != key){
          i++;
     }
-    //Checking the boundary.
-    if(i == number && key != testVector[i]){
-        cout << "The word you entered can not be found in the dictionary." << endl;
-    }
-    else{
     //printing out the index of the word you entered.
-        cout << "The index number of your word is: " << i << endl;
-    }
+    cout << "The index number of your word is: " << i << " by sequential search."<< endl;
     if(i < number){
         return true;
     }
@@ -67,12 +52,71 @@ int myTest(vector<string>testVector,int number,string key){
         return false;
     }
 }
+
+int sentinelSearch(vector<string>testVector, int number,string key){
+    testVector[number] = key;
+    int i = 0;
+    while(testVector[i] != key){
+        i++;
+    }
+    cout << "The index number of your word is: " << i << " by sentinel search." << endl;
+    if(i < number){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int probabilitySearch(vector<string>testVector, int number,string key){
+    int i = 0;
+    while(i < number && testVector[i] != key){
+        i++;
+    }
+    cout << "The index number of your word is: " << i << " by probability search." << endl;
+    if(i < number){
+        i = promote(testVector,number);
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+int promote(vector<string>testVector, int i){
+    string temp = testVector[i];
+    int j;
+    j = i - rand();
+    if(j < 0){
+        j = 0;
+    }
+    while(i > j){
+        testVector[i] = testVector[i - 1];
+        i--;
+    }
+    testVector[i] = temp;
+    return i;
+}
+int orderedSearch(vector<string>testVector, int number,string key){
+    int i = 0;
+    while(i < number && testVector[i] != key){
+        i++;
+    }
+    cout << "The index number of your word is: " << i << " by ordered search." << endl;
+    if(i < number && testVector[i] == key){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 //The following are some additional information about sequential search by Dr.Kramer.
 /*Sequential Search.
 Three questions about the algorithm.
-Question 1: What if the first list position is 1, not 0?
+Question 1: What if the first list position is 1, not 0?
 Yes, 
-If the first position is j , replace all occurrences of L[i ] with L[i + j ].
+If the first position is j , replace all occurrences of
+L[i ] with L[i + j ].
 
 Question 2: Can we always compare keys?
 Yes, 
@@ -85,4 +129,18 @@ Question 3: How do we indicate success and failure?
 Return valid index for success, invalid index for failure
 Return pointer to data if successful, NULL if not
 Either of the above if successful, throw an exception if not
+*/
+
+/* Sentinel Search Restrictions 
+Two restrictions on Sentinel Search.
+1. You must be able to add k to the end of the list and remove it if
+necessary. Must use Sequential Search otherwise.
+2. Adding and removing must be faster than O(n) time
+No faster than Sequential Search otherwise 
+*/
+
+/* Probability Search
+Probability Search comes in two favors:
+Static. - Boring, Arrange data before searching, and use sequential or sentinel search.
+Dynamic. - Start with sequential or sentinel search and if k is found, move it forward in the list.
 */
